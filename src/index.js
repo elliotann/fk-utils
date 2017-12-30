66bca35294ebd9430d73ad6a304d0d718c6d1967
andy <360042212@qq.com>
2017-12-30 11:27:14 +0800

    fdasf

---
+++ src/index.js
@@ -0,0 +1,35 @@
+import beautify from './beautify'
+import json from './json'
+import fetch from './fetch'
+import number from './number'
+import moment from './moment'
+import path from './path'
+import expression from './expression'
+import matrix from './matrix'
+import dom from './dom'
+import password from './password'
+import string from './string'
+import exception from './exception'
+import tree from './tree'
+import history from './history'
+import less from './less'
+import _ from 'lodash'
+
+export default {
+	beautify,
+	json,
+	fetch,
+	number,
+	string,
+	moment,
+	path,
+	expression,
+	matrix,
+	dom,
+	password,
+	exception,
+	tree,
+	history,
+	less,
+	_
+}
